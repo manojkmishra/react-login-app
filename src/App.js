c5c153954093ef049ae02d8932802f39588d5b28
Manoj <manojmishra571980@gmail.com>
2018-04-07 14:13:03 +1000

    Initial Login and Home page working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Route } from "react-router-dom";
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
 
+const App = () => (
+  <div>
+    <Route path="/" exact component ={HomePage} />
+    <Route path="/login" exact component ={LoginPage} />
+  </div>
+  );
+  
 
-const App = () => <div>Hello World</div>
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(  <BrowserRouter> <App /></BrowserRouter>, 
+ReactDOM.render(  <BrowserRouter> 
+                    <App />
+                    </BrowserRouter>, 
                    document.getElementById('root')
                );
 registerServiceWorker();
