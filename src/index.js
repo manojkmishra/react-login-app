7ae06e7d39be0bd5fb021d05347694bab50eb82c
Manoj <manojmishra571980@gmail.com>
2018-04-07 14:46:26 +1000

    added semantic ui, className=ui container for margins in app.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(  <BrowserRouter> 
-                    <App />
-                    </BrowserRouter>, 
-                   document.getElementById('root')
+                  <App />
+                  </BrowserRouter>, 
+                  document.getElementById('root')
                );
 registerServiceWorker();
