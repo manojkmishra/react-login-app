346d38a7861b93bd0709fc4015296d1789d96386
Manoj <manojmishra571980@gmail.com>
2018-04-08 18:30:34 +1000

    Logout button created - visible after login in home screen

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-const HomePage = () => (
-  <div>
-    <h1>Home Page</h1>
-    <Link to="/login">Login</Link>
+const HomePage = ({ isAuthenticated }) => 
+( <div> <h1>Home Page</h1>
+        {isAuthenticated ? <button>Logout</button> : <Link to="/login">Login</Link> }
   </div>
 );
 
-export default HomePage;
+HomePage.propTypes = {  isAuthenticated: PropTypes.bool.isRequired  };
+function mapStateToProps(state) {  return { isAuthenticated: !!state.user.token };  }
+
+export default connect(mapStateToProps)(HomePage);
