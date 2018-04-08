f6dc512c8ce3fbe39c9e40c4aed8cb4b815ac7ab
Manoj <manojmishra571980@gmail.com>
2018-04-08 19:22:30 +1000

    Logout working with store n reducers

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,9 +1,8 @@
-import {USER_LOGGED_IN} from '../types';
+import { USER_LOGGED_IN, USER_LOGGED_OUT } from "../types";
 import api from '../api';
 
-export const userLoggedIn = user =>
-({  type: USER_LOGGED_IN, user
-});
+export const userLoggedIn = user => ({  type: USER_LOGGED_IN, user  });
+export const userLoggedOut = () => ({   type: USER_LOGGED_OUT   });
 
 //thunk actions
 export const login = credentials => dispatch =>
@@ -11,5 +10,11 @@ export const login = credentials => dispatch =>
     localStorage.bookwormJWT = user.token;
     dispatch(userLoggedIn(user));
 });
+
+export const logout = () => dispatch => {
+    localStorage.removeItem("bookwormJWT");
+ 
+    dispatch(userLoggedOut());
+  };
       
     
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import * as actions from "../../actions/auth";
 
-const HomePage = ({ isAuthenticated }) => 
+const HomePage = ({ isAuthenticated, logout }) => 
 ( <div> <h1>Home Page</h1>
-        {isAuthenticated ? <button>Logout</button> : <Link to="/login">Login</Link> }
+        {isAuthenticated ? (<button onClick={() => logout()}>Logout</button>) : 
+(<Link to="/login">Login</Link> )}  
   </div>
 );
 
-HomePage.propTypes = {  isAuthenticated: PropTypes.bool.isRequired  };
+HomePage.propTypes = {  isAuthenticated: PropTypes.bool.isRequired,  logout: PropTypes.func.isRequired  };
 function mapStateToProps(state) {  return { isAuthenticated: !!state.user.token };  }
 
-export default connect(mapStateToProps)(HomePage);
+export default connect(mapStateToProps, { logout: actions.logout })(HomePage);
