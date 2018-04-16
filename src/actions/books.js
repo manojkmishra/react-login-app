ed67ea27c86a72b4339ebc0de8b93fca9cdfe068
manojkmishra <manojkm1980@gmail.com>
2018-04-17 07:46:41 +1000

    (bug resolved)- books not coming after login if user has books

--- src/actions/books.js
+++ src/actions/books.js
@@ -7,20 +7,18 @@ import { bookSchema } from "../schemas";
 
 // data.entities.books
 
-
+const booksFetched = data => ({  type: BOOKS_FETCHED,  data});
 const bookCreated = data => ({  type: BOOK_CREATED,  data});
 
 export const fetchBooks = () => dispatch =>
-  api.books.fetchAll().then(books =>{ dispatch(booksFetched(normalize(books, [bookSchema])));
-
-    console.log('all the books of this user', normalize(books, [bookSchema]));
-
-  }
-
-);
+  api.books.fetchAll().then(books =>
+                              { dispatch(booksFetched(normalize(books, [bookSchema])));
+                                console.log('all the books of this user', normalize(books, [bookSchema]));
+                              }
+                          );
   //dispatch is required because we want the result to place in out redux store
   //books passed to booksFetched---so wee need to have it from api file--and it should be normalized
 
-  const booksFetched = data => ({  type: BOOKS_FETCHED,  data});
+
 export const createBook = data => dispatch =>
   api.books.create(data).then(book => dispatch(bookCreated(normalize(book, bookSchema))));
